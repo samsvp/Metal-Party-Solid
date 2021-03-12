f2e1ced1ee538562459999d1171425a6cd9a996b
Vitor Pavani <vitorpavani@poli.ufrj.br>
2021-03-11 22:59:51 -0300

    fix fullscreen navigation

--- Node2D.gd
+++ Node2D.gd
@@ -5,18 +5,19 @@ onready var line_2d : Line2D = $Line2D
 onready var character : Sprite = $Sprite
 
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
 	
 func _unhandled_input(event: InputEvent):
+	
 	if not event is InputEventMouseButton:
 		return
 	if event.button_index != BUTTON_LEFT or not event.pressed:
 		return
 	
-	var new_path = nav_2d.get_simple_path(character.global_position, event.global_position)
+	var new_path = nav_2d.get_simple_path(character.global_position, get_viewport().get_mouse_position())
 	line_2d.points = new_path
 	character.path = new_path
 	print(new_path)
-	
--- Sprite.gd
+++ Sprite.gd
@@ -4,6 +4,11 @@ extends Sprite
 var speed = 400.0
 var path = PoolVector2Array() setget set_path
 
+func set_path(value):
+	path = value
+	if value.size() == 0:
+		return
+	set_process(true)
 
 func _ready():
 	set_process(false)
@@ -11,7 +16,7 @@ func _ready():
 func _process(delta):
 	var move_distance = speed * delta
 	move_along_path(move_distance)
-	
+
 func move_along_path(distance):
 	var start_point = position
 	for i in range(path.size()):
@@ -19,17 +24,13 @@ func move_along_path(distance):
 		if distance <= distance_to_next and distance >= 0.0:
 			position = start_point.linear_interpolate(path[0], distance/distance_to_next)
 			break
-		elif distance < 0.0:
+		elif path.size() == 1 && distance > distance_to_next:
 			position = path[0]
 			set_process(false)
+			
+			## path end
 			break
+			
 		distance -= distance_to_next
 		start_point = path[0]
 		path.remove(0)
-
-func set_path(value):
-	path = value
-	if value.size() == 0:
-		return
-	set_process(true)
-	
