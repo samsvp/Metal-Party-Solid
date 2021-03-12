602a590e3ae37b0d16658fb992362f641e25ca9f
Pedro  Teixeira <pedroteixeir@me.com>
2021-03-12 02:27:36 -0300

    Added Player movement

---
+++ Player.gd
@@ -0,0 +1,26 @@
+extends KinematicBody2D
+
+
+# Declare member variables here. Examples:
+const MOVE_SPEED = 300
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta):
+	var move_vec = Vector2()
+	if Input.is_action_pressed("ui_up"):
+		move_vec.y -= 1
+	if Input.is_action_pressed("ui_down"):
+		move_vec.y += 1
+	if Input.is_action_pressed("ui_left"):
+		move_vec.x -= 1
+	if Input.is_action_pressed("ui_right"):
+		move_vec.x += 1
+	move_vec = move_vec.normalized()
+	move_and_collide(move_vec * MOVE_SPEED * delta)
