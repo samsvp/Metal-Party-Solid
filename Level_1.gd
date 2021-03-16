f1a5e99abc6060f607968951e37eea4c3ed16ae6
Pedro  Teixeira <pedroteixeir@me.com>
2021-03-16 04:25:42 -0300

    Disabled enemies between levels

--- Level_1.gd
+++ Level_1.gd
@@ -1,5 +1,5 @@
 extends Area2D
-
+signal disable_enemies_1(value)
 export var destination = Vector2(0,0)
 # Declare member variables here. Examples:
 # var a = 2
@@ -9,3 +9,4 @@ export var destination = Vector2(0,0)
 func _on_Level_1__body_entered(body):
 	if body.is_in_group("Player"):
 		body.position = destination
+		emit_signal("disable_enemies_1",true)
--- enemy_patrol.gd
+++ enemy_patrol.gd
@@ -15,7 +15,12 @@ func _process(delta):
 	seeing_player(player.position)
 	var move_distance = speed * delta
 	move(move_distance)
+	
 
 func after_move():
 	var target = curve.get_point_position(randi() % curve.get_point_count())
 	set_target_pos(target)
+
+
+func _on_Level_1__disable_enemies_1(value):
+	set_process(false)
