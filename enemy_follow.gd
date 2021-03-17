11df9f0b8467f4923fb2fe9b8599a25137e0f474
Vitor Pavani <vitorpavani@poli.ufrj.br>
2021-03-17 02:57:07 -0300

    enemy wont push the player

--- enemy_follow.gd
+++ enemy_follow.gd
@@ -1,14 +1,15 @@
 extends "animated_enemy.gd"
 
+export var min_distance = 50.0
+	
 func _ready():
 	set_target_pos(player.position)
 
-
 func _process(delta):
-	seeing_player()
 	var move_distance = speed * delta
-	move(move_distance)
 	
-
-func after_move():
-	set_target_pos(player.position)
+	if seeing_player():
+		set_target_pos(player.position)
+	
+	if self.position.distance_to(player.position) > min_distance:
+		move(move_distance)
